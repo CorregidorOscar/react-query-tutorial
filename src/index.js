dc40058245e0231650c6e1bd30216c337d172a56
CorregidorOscar <corregidoroscar18@gmail.com>
2022-02-08 16:11:49 -0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//react-query
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+const queryClient = new QueryClient();
+ReactDOM.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+    <ReactQueryDevtools />
+  </QueryClientProvider>,
+  document.getElementById("root")
+);
